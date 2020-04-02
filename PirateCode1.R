e22ede4ee2f1fbf5883d2df914001bf4e481555c
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-04-02 14:44:38 -0700

    2020.04.02 saved both bits of R script under unique names.

---
+++ PirateCode1.R
@@ -0,0 +1,20 @@
+##installing the yarr package and continuing with tutorial
+
+install.packages('yarrr')
+
+library(yarrr)
+library(yarr)
+??pirates 
+?pirates
+
+##I want to see first rows of the dataframe here...
+
+head(pirates)
+
+##I want names of columns 
+names(pirates)
+
+##View all - but notice how View has to be capitalized
+View(pirates)
+
+
---
+++ PirateCodeOrigins.R
@@ -0,0 +1,25 @@
+#2020.04.02 Tutorials
+
+##I'm learning about code chunks
+
+farce <- 1:29
+
+##print farce
+farce
+
+##what is the mean of farce?
+mean(farce)
+
+##let's see how elements are numbered when asked for output
+
+seq(from = 0, to = 120, by = 3)
+##R is not ready (+)
+## (+) symbol means that R is Waiting for you to (properly) finish code you started before
+##R is ready (>)
+
+##playing with commas and periods
+mean(c(22557, 4520, 2))
+#mean(c(22557, 4520. 2))
+##error you should see Error: unexpected numeric constant in "mean(c(22557, 4520. 2"
+
+

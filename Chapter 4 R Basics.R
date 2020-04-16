9e5395d4d905dcf09b2d69eb71e97192cd649c8b
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-04-16 12:28:36 -0700

    20202.04.16 separating Chapter 4 from Yarr pirate guide book

---
+++ Chapter 4 R Basics.R
@@ -0,0 +1,29 @@
+## Hello World
+## Chaptter 4 R Basics
+
+
+#*********************************
+##Libraries
+#*********************************
+library(yarrr)
+library(yarr)
+library(BayesFactor)
+
+#*********************************
+##Checking underlying dataframe
+#*********************************
+
+dim(pirates)
+head(pirates)
+
+
+#*********************************
+## 4.1 Command Line- The Console
+#*********************************
+
+## symbol in brackets is called a prompt [ > ]
+1 + 1
+## view from the console:
+## > 1 + 1
+## [1] 2
+## > 

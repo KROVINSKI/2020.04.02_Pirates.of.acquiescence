af37bc05f85dee13b19c0c2e03062ffcfa3398d4
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-04-27 15:34:18 -0700

    2020.04.27 1530PST concatenating

--- Chapter 5 R Scalars and Vectors.R
+++ Chapter 5 R Scalars and Vectors.R
@@ -67,11 +67,28 @@ f <- "Do any modern armies still use cannons?"
 # there are many ways to create a vector, many way to use a function to create a vector
 # methods we will cover in this chapter:
 
-# c(a, b, ...)  conc
+# c(a, b, ...)  concatenate
+# a:b (imagine using with scalars like 1:5, 1 in numerical sequence to 5)
+# seq(from, to, by, length.out) , seq(from = 0, to = 6, by = 2), resulting in 0, 2, 4, 6
+# replicates - rep(x, times, each, length.out), rep(c(7, 8), times = 2, each = 2), resulting in 7, 7, 8, 8, 7, 7, 8, 8
 
+# 5.2 Creating a vector 
+# Create an object a with the integers from 1 to 5
 
+a <- (1:5)
+#print results 
+a
 
+# Create an object b with the integers from 6 to 10
+b <- (6:10)
+#print results
+b
 
+# Create an object by combining into a singular vector:
+# use the function concatenate
+x <- c(a,b)
+#print results
+x
 
 
 

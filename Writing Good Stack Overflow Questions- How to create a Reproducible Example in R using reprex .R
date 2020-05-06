667124bd7fadaa4e2ef332a2ef86ebd2dec1ce52
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-05-06 09:58:29 -0700

    2020.05.06 0956PST finally getting the libraries in correctly

--- Writing Good Stack Overflow Questions- How to create a Reproducible Example in R using reprex .R
+++ Writing Good Stack Overflow Questions- How to create a Reproducible Example in R using reprex .R
@@ -7,13 +7,26 @@
 ##Libraries
 #*********************************
 install.packages("reprex")
-
+## devtools::install_github("tidyverse/reprex")
 reprex
 
+#*********************************
+## Example Dataframe
+#*********************************
 
-#*********************************
-## Setting working directory
-#*********************************
+(y <- 1:4)
+mean(y)
+# Results =
+# > (y <- 1:4)
+# [1] 1 2 3 4
+# > mean(y)
+# [1] 2.5
+
+
+reprex(y)
+
+
+
 
 # 
 # 

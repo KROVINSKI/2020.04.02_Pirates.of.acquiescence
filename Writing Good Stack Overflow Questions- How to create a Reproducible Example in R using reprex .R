df1c13f713135d521289e28d212556b91aa1b1e9
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-05-06 09:35:58 -0700

    2020.05.06 0935PST Writing Good Stack Overflow Questions: How to create a Reproducible Example in R using reprex

---
+++ Writing Good Stack Overflow Questions- How to create a Reproducible Example in R using reprex .R
@@ -0,0 +1,24 @@
+## Hello World
+## Author: Pirate Captain Kate with trusty Gus 
+## Title: Writing Good Stack Overflow Questions: How to create a Reproducible Example in R using reprex 
+## Date: May 2020
+
+#*********************************
+##Libraries
+#*********************************
+
+
+
+
+# 
+# 
+# 
+
+
+
+
+
+
+#*********************************
+## End of Script | End of Document
+#*********************************
